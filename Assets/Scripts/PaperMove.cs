b97905f463ac170c8878aab8621ac8a0e9f7ba78
Emanuel Umbelino <emanuel.umbelin@gmail.com>
2016-09-30 13:02:38 -0300

    Best Movimentation

--- Assets/Scripts/PaperMove.cs
+++ Assets/Scripts/PaperMove.cs
@@ -4,60 +4,69 @@ using System;
 
 public class PaperMove : MonoBehaviour
 {
-
-    public float minSwipeDistY;
-
-    public float minSwipeDistX;
-
-    private Vector2 startPos;
-
-
     [SerializeField]
     private float acelerationV;
     [SerializeField]
     private float decelerationV;
+    [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
+    private Rigidbody rb;
     private float speedX;
     private float speedY;
 
     void Update()
     {
         SpeedUp();
-        transform.Translate(new Vector2(speedX, speedY));
-        speedX = deceleration(speedX); speedY = deceleration(speedY);
+        rb.AddForce(new Vector2(speedX, speedY));
+        speedX = deceleration(speedX,true); speedY = deceleration(speedY,false);
     }
 
     void SpeedUp()
     {
-        if (Input.GetAxis("Vertical") > 0)
+        //print("x :  " + speedX + "  y :  " + speedY);
+        //print("x :  " + rb.velocity.x + "  y :  " + rb.velocity.y);
+        if (Input.GetAxis("Vertical") > 0 && rb.velocity.y < maxSpeed)
         {
-            speedY += acelerationV * Time.deltaTime;
+            rb.AddForce(new Vector2(0, acelerationV * Time.deltaTime));
 
         }
-        if (Input.GetAxis("Vertical") < 0)
+        if (Input.GetAxis("Vertical") < 0 && rb.velocity.y > -maxSpeed)
         {
-            speedY -= acelerationV * Time.deltaTime;
+            rb.AddForce(new Vector2(0, -acelerationV * Time.deltaTime));
         }
-        if (Input.GetAxis("Horizontal") > 0)
+        if (Input.GetAxis("Horizontal") > 0 && rb.velocity.x < maxSpeed)
         {
-            speedX += acelerationV * Time.deltaTime;
+            rb.AddForce(new Vector2(acelerationV * Time.deltaTime, 0));
         }
-        if (Input.GetAxis("Horizontal") < 0)
+        if (Input.GetAxis("Horizontal") < 0 && rb.velocity.x > -maxSpeed)
         {
-            speedX -= acelerationV * Time.deltaTime;
+            rb.AddForce(new Vector2(-acelerationV * Time.deltaTime, 0));
         }
     }
-    float deceleration(float axis)
+    float deceleration(float axis, bool x)
     {
         if (axis != 0)
         {
             if (axis > 0)
+            {
                 axis -= decelerationV * Time.deltaTime;
+                if(x)
+                    rb.AddForce(new Vector2(decelerationV * Time.deltaTime, 0));
+                else
+                    rb.AddForce(new Vector2(0, decelerationV * Time.deltaTime));
+            }
             if (axis < 0)
+            {
                 axis += decelerationV * Time.deltaTime;
+                if (x)
+                    rb.AddForce(new Vector2(-decelerationV * Time.deltaTime, 0));
+                else
+                    rb.AddForce(new Vector2(0, -decelerationV * Time.deltaTime));
+            }
         }
         if (axis > -0.001f && axis < 0.001f)
             axis = 0;
-
         return axis;
     }
 }
